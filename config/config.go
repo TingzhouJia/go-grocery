7fcc3ad48eaa7a4fa8b05a6ee006a5b0e51ab70e
TingzhouJia <joe.tzjia@gmail.com>
2021-01-30 23:01:32 -0500

    config

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"log"
+)
+type Config struct {
+	Name string
+}
+
+func (c *Config) init() error {
+	if c.Name !=""{
+		viper.SetConfigFile(c.Name)
+	}else{
+		viper.AddConfigPath("conf")
+		viper.SetConfigName("config")
+	}
+	viper.SetConfigType("yaml")
+	if err:=viper.ReadInConfig();err!=nil{
+		return err
+	}
+	return nil
+}
+
+func (c *Config) watchConfig()  {
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		log.Printf("log change")
+	})
+}
+
+func Init(name string)  error{
+	c:=Config{Name: name}
+	if err:=c.init();err!=nil{
+		return err
+	}
+	c.watchConfig()
+	return nil
+}
+
