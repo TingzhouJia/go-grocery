4a1ac98326a768f45a1815c68baf3e88508b71ac
TingzhouJia <joe.tzjia@gmail.com>
2021-01-31 23:06:42 -0500

    dto

--- dto/category.go
+++ dto/category.go
@@ -1,1 +1,30 @@
 package dto
+
+type BaseCategory struct {
+	CategoryID string                `json:"categoryID"`
+	Name       string                `json:"name"`
+	Desc       string                `json:"desc"`
+	Order      int                   `json:"order"`
+	ParentID   string                `json:"parentId"`
+	Children   map[string]*SubCategory `json:"children"`
+}
+
+type SubCategory struct {
+	CategoryID string                `json:"categoryID"`
+	Name       string                `json:"name"`
+	Desc       string                `json:"desc"`
+	Order      int                   `json:"order"`
+	ParentID   string                `json:"parentId"`
+	Children   map[string]*ChildCategory `json:"children"`
+}
+
+type ChildCategory struct {
+	Key        string `json:"key"`
+	Id         string `json:"id"`
+	CategoryID string `json:"categoryID"`
+	Name       string `json:"name"`
+	Desc       string `json:"desc"`
+	Order      int    `json:"order"`
+	ParentID   string `json:"parentId"`
+	IsDeleted  bool   `json:"isDeleted"`
+}
