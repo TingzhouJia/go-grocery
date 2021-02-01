8cd4522210d77df32d881fa90f06df341faf33ca
TingzhouJia <joe.tzjia@gmail.com>
2021-01-31 23:46:04 -0500

    controller

--- controller/user.go
+++ controller/user.go
@@ -1,1 +1,18 @@
 package controller
+
+
+
+//func UserController(engine *gin.Engine) *gin.RouterGroup {
+//	user:=engine.Group("/api/user")
+//	{
+//		user.GET("/list",UserBaseHandler)
+//		user.GET("/:id",grocery.UserBaseHandler.GetUserInfo)
+//		user.POST("/",grocery.UserBaseHandler.AddUserHandler)
+//		user.PUT("/",grocery.UserBaseHandler.EditUserHandler)
+//		user.DELETE("/",grocery.UserBaseHandler.DeleteUserHandler)
+//
+//	}
+//	return user
+//}
+
+
