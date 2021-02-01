c101dd9485d2af66b27867dabcdb2280448234f2
TingzhouJia <joe.tzjia@gmail.com>
2021-01-31 23:46:14 -0500

    enum

--- enum/responseEnum.go
+++ enum/responseEnum.go
@@ -1,1 +1,31 @@
 package enum
+
+type ResStatus int
+
+const (
+	ServerError ResStatus =500
+	UnAuthorized ResStatus=403
+	NotFound ResStatus=404
+	Success ResStatus =200
+	Accepted ResStatus=201
+	DeleteSuccess ResStatus=204
+)
+
+func (receiver ResStatus) String() string {
+	switch receiver {
+	case ServerError:
+		return "server error"
+	case UnAuthorized:
+		return "unauthorized access"
+	case NotFound:
+		return "resource not found"
+	case Success:
+		return "get resource successful"
+	case Accepted:
+		return "resource action accepted"
+	case DeleteSuccess:
+		return "remove resource successful"
+	default:
+		return "unknown error"
+	}
+}
