3acfe0886fea03474f7fc21771d6334ae5605143
TingzhouJia <joe.tzjia@gmail.com>
2021-02-04 23:07:43 -0500

    user

--- user-service/main.go
+++ user-service/main.go
@@ -6,23 +6,36 @@ import (
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/registry/etcd"
+	"github.com/micro/go-plugins/config/source/grpc/v2"
 	"grocery/basic"
+	"grocery/basic/common"
 	"grocery/basic/config"
 	"grocery/user-service/handler"
 	"grocery/user-service/model"
 	pb "grocery/user-service/proto"
 )
 
+var (
+	appName = "user_srv"
+	cfg     = &userCfg{}
+)
+
+type userCfg struct {
+	common.AppCfg
+}
+
+
 func main() {
-	basic.Init()
+	initCfg()
 
 	microReg:=etcd.NewRegistry(registryOptions)
 
 	// Create service
 	srv := micro.NewService(
-		micro.Name("grocery.user-service"),
+		micro.Name(cfg.Name),
 		micro.Registry(microReg),
-		micro.Version("latest"),
+		micro.Version(cfg.Version),
+		micro.Address(cfg.Addr()),
 	)
 
 	srv.Init(
@@ -40,8 +53,30 @@ func main() {
 
 	}
 }
-
 func registryOptions(ops *registry.Options) {
-	etcdCfg := config.GetEtcdConfig()
-	ops.Addrs = []string{fmt.Sprintf("%s:%d", etcdCfg.GetHost(), etcdCfg.GetPort())}
+	etcdCfg := &common.Etcd{}
+	err := config.C().App("etcd", etcdCfg)
+	if err != nil {
+		panic(err)
+	}
+	ops.Addrs = []string{fmt.Sprintf("%s:%d", etcdCfg.Host, etcdCfg.Port)}
 }
+
+func initCfg() {
+	source := grpc.NewSource(
+		grpc.WithAddress("127.0.0.1:9600"),
+		grpc.WithPath("micro"),
+	)
+
+	basic.Init(config.WithSource(source))
+
+	err := config.C().App(appName, cfg)
+	if err != nil {
+		panic(err)
+	}
+
+
+
+	return
+}
+
