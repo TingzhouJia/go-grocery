96de50c2403f29979fa48fbdc21f47502eab541a
TingzhouJia <joe.tzjia@gmail.com>
2021-02-04 23:06:33 -0500

    inventory service

--- inventory-srv/main.go
+++ inventory-srv/main.go
@@ -7,25 +7,35 @@ import (
 	"github.com/micro/go-micro/v2/logger"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/registry/etcd"
+	"github.com/micro/go-plugins/config/source/grpc/v2"
 	"grocery/basic"
+	"grocery/basic/common"
 	"grocery/basic/config"
 	"grocery/inventory-srv/handler"
 	"grocery/inventory-srv/model"
 	pb "grocery/inventory-srv/proto"
 )
 
-func main() {
+var (
+	appName = "inv_srv"
+	cfg     = &appCfg{}
+)
 
+type appCfg struct {
+	common.AppCfg
+}
+func main() {
+	initCfg()
 	reg:=etcd.NewRegistry(registryOptions)
 
 	// Create service
 	srv := micro.NewService(
-		micro.Name("inventory-srv"),
-		micro.Version("latest"),
+		micro.Name(cfg.Name),
+		micro.Version(cfg.Version),
 		micro.Registry(reg),
+		micro.Address(cfg.Addr()),
 	)
 
-	basic.Init()
 
 	srv.Init(
 		micro.Action(func(context *cli.Context) error {
@@ -44,6 +54,29 @@ func main() {
 }
 
 func registryOptions(ops *registry.Options) {
-	etcdCfg := config.GetEtcdConfig()
-	ops.Addrs = []string{fmt.Sprintf("%s:%d", etcdCfg.GetHost(), etcdCfg.GetPort())}
+	etcdCfg := &common.Etcd{}
+	err := config.C().App("etcd", etcdCfg)
+	if err != nil {
+		panic(err)
+	}
+
+	ops.Addrs = []string{fmt.Sprintf("%s:%d", etcdCfg.Host, etcdCfg.Port)}
+}
+
+func initCfg() {
+	source := grpc.NewSource(
+		grpc.WithAddress("127.0.0.1:9600"),
+		grpc.WithPath("micro"),
+	)
+
+	basic.Init(config.WithSource(source))
+
+	err := config.C().App(appName, cfg)
+	if err != nil {
+		panic(err)
+	}
+
+
+
+	return
 }
