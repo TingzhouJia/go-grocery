a715380eaa4480332d38b4eb49c790491bdf00b9
TingzhouJia <joe.tzjia@gmail.com>
2021-02-04 23:04:02 -0500

    pay web handler

--- payment-web/handler/handler.go
+++ payment-web/handler/handler.go
@@ -1,1 +1,69 @@
 package handler
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/micro/go-micro/v2/client"
+	log "github.com/micro/go-micro/v2/logger"
+	auth "grocery/auth/proto"
+	payment "grocery/payment-service/proto"
+	"net/http"
+	"strconv"
+	"time"
+)
+var (
+	serviceClient payment.PaymentService
+	authClient    auth.Service
+)
+type Error struct {
+	Code   string `json:"code"`
+	Detail string `json:"detail"`
+}
+
+func Init() {
+	serviceClient = payment.NewPaymentService("mu.micro.book.srv.payment", client.DefaultClient)
+	authClient = auth.NewService("mu.micro.book.srv.auth", client.DefaultClient)
+}
+
+// PayOrder 支付订单
+func PayOrder(w http.ResponseWriter, r *http.Request) {
+
+	// 只接受POST请求
+	if r.Method != "POST" {
+		log.Warn("非法请求")
+		http.Error(w, "非法请求", 400)
+		return
+	}
+
+	r.ParseForm()
+
+	orderId, _ := strconv.ParseInt(r.Form.Get("orderId"), 10, 10)
+
+	// 调用后台服务
+	_, err := serviceClient.PayOrder(context.TODO(), &payment.Request{
+		OrderId: orderId,
+	})
+
+	// 返回结果
+	response := map[string]interface{}{}
+
+	// 返回结果
+	response["ref"] = time.Now().UnixNano()
+	if err != nil {
+		response["success"] = false
+		response["error"] = Error{
+			Detail: err.Error(),
+		}
+	} else {
+		response["success"] = true
+	}
+
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+
+	// 返回JSON结构
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
