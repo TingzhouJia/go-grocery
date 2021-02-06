850a714955b681d8e031651841baaddb8c05ce65
TingzhouJia <joe.tzjia@gmail.com>
2021-02-05 23:49:16 -0500

    msin

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
-package grocery
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"grocery/middleware"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(middleware.Cors())
+	gin.SetMode(viper.GetString("mode"))
+	user := r.Group("/api/user")
+	{
+		user.GET("/list", UserBaseHandler.UserListHandler)
+		user.GET("/:id", UserBaseHandler.GetUserInfo)
+		user.POST("/", UserBaseHandler.AddUserHandler)
+		user.PUT("/", UserBaseHandler.EditUserHandler)
+		user.DELETE("/", UserBaseHandler.DeleteUserHandler)
+
+	}
+	port:=viper.GetString("port")
+	r.Run(port)
+}
